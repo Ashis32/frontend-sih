cff644613a8bc16ff2483e610de9c64e7cbe834e
Ashis32 <ashiskumarbehura32@gmail.com>
2024-09-11 20:43:48 +0530

    commit1

---
+++ web/src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Plant AI",
+  url: "https://github.com/Ashis32",
+  ogImage: "https://github.com/Ashis32",
+  description:
+    "Your digital gateway to the botanical world. Unearth a wealth of insights, from species details to disease detection, effortlessly bridging the gap between technology and the beauty of nature.",
+  links: {
+    twitter: "https://github.com/Ashis32",
+    github: "https://github.com/Ashis32",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
